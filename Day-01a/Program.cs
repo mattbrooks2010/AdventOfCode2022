f090d5ef5c14e5637682560c2f22e2c1350a87f3
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-01 08:23:48 +0000

    Day 1

---
+++ Day-01a/Program.cs
@@ -0,0 +1,18 @@
+﻿var calories = 0;
+var max = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    if (line.Length > 0)
+    {
+        calories += int.Parse(line);
+        max = Math.Max(max, calories);
+    }
+    else
+    {
+        calories = 0;
+    }
+}
+
+Console.WriteLine(max);
---
+++ Day-01b/Program.cs
@@ -0,0 +1,18 @@
+﻿var totals = new int[1];
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    if (line.Length > 0)
+    {
+        totals[^1] += int.Parse(line);
+    }
+    else
+    {
+        Array.Resize(ref totals, totals.Length + 1);
+    }
+}
+
+Array.Sort(totals);
+
+Console.WriteLine(totals[^3..].Sum());
