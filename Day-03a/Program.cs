93bb50a54a284b023192a2bf1a4a717573c5f7dc
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-03 07:53:02 +0000

    Day 3

---
+++ Day-03a/Program.cs
@@ -0,0 +1,27 @@
+﻿var priority = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    priority += GetPriority(line);
+}
+
+int GetPriority(string line)
+{
+    for (var i = 0; i < line.Length / 2; i++)
+    {
+        for (var j = line.Length / 2; j < line.Length; j++)
+        {
+            var item = line[i];
+
+            if (item == line[j])
+            {
+                return item - (char.IsUpper(item) ? 38 : 96);
+            }
+        }
+    }
+
+    return 0;
+}
+
+Console.WriteLine(priority);
---
+++ Day-03b/Program.cs
@@ -0,0 +1,37 @@
+﻿var priority = 0;
+var group = new List<string>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    group.Add(line);
+    
+    if (group.Count == 3)
+    {
+        priority += GetPriority(group);
+        group.Clear();
+    }
+}
+
+int GetPriority(List<string> group)
+{
+    for (var i = 0; i < group[0].Length; i++)
+    {
+        for (var j = 0; j < group[1].Length; j++)
+        {
+            for (var k = 0; k < group[2].Length; k++)
+            {
+                var item = group[0][i];
+
+                if (item == group[1][j] && item == group[2][k])
+                {
+                    return item - (char.IsUpper(item) ? 38 : 96);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+Console.WriteLine(priority);
