40e4bc33767164279167b86d49661ebada1b9844
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-04 09:11:13 +0000

    Day 4

---
+++ Day-04b/Program.cs
@@ -0,0 +1,18 @@
+﻿var count = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var ranges = line.Split(new[] { '-', ',' });
+    var a = int.Parse(ranges[0]);
+    var b = int.Parse(ranges[1]);
+    var c = int.Parse(ranges[2]);
+    var d = int.Parse(ranges[3]);
+
+    if ((b >= c && a <= d) || (a <= d && b >= c))
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
