976ef55e7e8172578e0d045b80f28729814f956f
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-06 05:14:14 +0000

    Day 6

---
+++ Day-06b/Program.cs
@@ -0,0 +1,22 @@
+﻿var pos = 0;
+var buffer = new List<char>();
+var bufferSize = 14;
+var read = char.MinValue;
+
+while ((read = (char)Console.Read()) != -1)
+{
+    buffer.Add(read);
+    pos++;
+    
+    if (buffer.Count > bufferSize)
+    {
+        buffer.RemoveAt(0);
+    }
+    
+    if (buffer.Distinct().Count() == bufferSize)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(pos);
