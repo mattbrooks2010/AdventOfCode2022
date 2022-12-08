4bcdda2c812e9a7433f6c3d14ad1be3a376c3966
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-08 07:16:42 +0000

    Day 8

---
+++ Day-08a/Program.cs
@@ -0,0 +1,90 @@
+﻿var map = new int[99, 99];
+var count = (map.GetLength(0) * 2) + (map.GetLength(1) * 2) - 4; // outer edge
+var lineIndex = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    for (var i = 0; i < map.GetLength(0); i++)
+    {
+        map[i, lineIndex] = Convert.ToInt32(line[i]);
+    }
+
+    lineIndex++;
+}
+
+for (var i = 1; i < map.GetLength(0) - 1; i++)
+{
+    for (var j = 1; j < map.GetLength(1) - 1; j++)
+    {
+        var height = map[i, j];
+        var visible = false;
+
+        // left
+        for (var k = i - 1; k >= 0; k--)
+        {
+            if (map[k, j] >= height)
+            {
+                break;
+            }
+            else if (k == 0)
+            {
+                visible = true;
+                count++;
+            }
+        }
+
+        if (!visible)
+        {
+            // right
+            for (var k = i + 1; k < map.GetLength(0); k++)
+            {
+                if (map[k, j] >= height)
+                {
+                    break;
+                }
+                else if (k == map.GetLength(0) - 1)
+                {
+                    visible = true;
+                    count++;
+                }
+            }
+        }
+
+        if (!visible)
+        {
+            // up
+            for (var k = j - 1; k >= 0; k--)
+            {
+                if (map[i, k] >= height)
+                {
+                    break;
+                }
+                else if (k == 0)
+                {
+                    visible = true;
+                    count++;
+                }
+            }
+        }
+
+        if (!visible)
+        {
+            // down
+            for (var k = j + 1; k < map.GetLength(1); k++)
+            {
+                if (map[i, k] >= height)
+                {
+                    break;
+                }
+                else if (k == map.GetLength(1) - 1)
+                {
+                    visible = true;
+                    count++;
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine(count);
---
+++ Day-08b/Program.cs
@@ -0,0 +1,74 @@
+﻿var map = new int[99, 99];
+var score = 0;
+var lineIndex = 0;
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    for (var i = 0; i < map.GetLength(0); i++)
+    {
+        map[i, lineIndex] = Convert.ToInt32(line[i]);
+    }
+
+    lineIndex++;
+}
+
+for (var i = 1; i < map.GetLength(0) - 1; i++)
+{
+    for (var j = 1; j < map.GetLength(1) - 1; j++)
+    {
+        var height = map[i, j];
+        var up = 0;
+        var down = 0;
+        var left = 0;
+        var right = 0;
+
+        // left
+        for (var k = i - 1; k >= 0; k--)
+        {
+            left++;
+
+            if (map[k, j] >= height)
+            {
+                break;
+            }
+        }
+
+        // right
+        for (var k = i + 1; k < map.GetLength(0); k++)
+        {
+            right++;
+
+            if (map[k, j] >= height)
+            {
+                break;
+            }
+        }
+
+        // up
+        for (var k = j - 1; k >= 0; k--)
+        {
+            up++;
+
+            if (map[i, k] >= height)
+            {
+                break;
+            }
+        }
+
+        // down
+        for (var k = j + 1; k < map.GetLength(1); k++)
+        {
+            down++;
+
+            if (map[i, k] >= height)
+            {
+                break;
+            }
+        }
+
+        score = Math.Max(score, up * down * left * right);
+    }
+}
+
+Console.WriteLine(score);
