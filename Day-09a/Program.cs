eb2d003482464f98192737befb0176266bcb82d6
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-09 23:48:29 +0000

    Day 9

---
+++ Day-09a/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+
+var rope = new Point[2];
+var visited = new HashSet<Point>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var direction = line[0];
+    var distance = int.Parse(line.Substring(2));
+
+    switch (direction)
+    {
+        case 'U':
+            Move(0, 1, distance);
+            break;
+        case 'D':
+            Move(0, -1, distance);
+            break;
+        case 'L':
+            Move(-1, 0, distance);
+            break;
+        case 'R':
+            Move(1, 0, distance);
+            break;
+    }
+}
+
+void Move(int x, int y, int distance)
+{
+    rope[0].X += x;
+    rope[0].Y += y;
+
+    var offset = new
+    {
+        X = rope[0].X - rope[1].X,
+        Y = rope[0].Y - rope[1].Y
+    };
+
+    void MoveX()
+    {
+        if (offset.X > 0)
+        {
+            rope[1].X++;
+        }
+        else if (offset.X < 0)
+        {
+            rope[1].X--;
+        }
+    }
+
+    void MoveY()
+    {
+        if (offset.Y > 0)
+        {
+            rope[1].Y++;
+        }
+        else if (offset.Y < 0)
+        {
+            rope[1].Y--;
+        }
+    }
+
+    if (offset.X > 1)
+    {
+        rope[1].X++;
+        MoveY();
+    }
+    else if (offset.X < -1)
+    {
+        rope[1].X--;
+        MoveY();
+    }
+    else if (offset.Y > 1)
+    {
+        rope[1].Y++;
+        MoveX();
+    }
+    else if (offset.Y < -1)
+    {
+        rope[1].Y--;
+        MoveX();
+    }
+
+    visited.Add(rope[^1]);
+
+    if (distance > 1)
+    {
+        Move(x, y, distance - 1);
+    }
+}
+
+Console.WriteLine(visited.Count);
---
+++ Day-09b/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Drawing;
+
+var rope = new Point[10];
+var visited = new HashSet<Point>();
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var direction = line[0];
+    var distance = int.Parse(line.Substring(2));
+
+    switch (direction)
+    {
+        case 'U':
+            Move(0, 1, distance);
+            break;
+        case 'D':
+            Move(0, -1, distance);
+            break;
+        case 'L':
+            Move(-1, 0, distance);
+            break;
+        case 'R':
+            Move(1, 0, distance);
+            break;
+    }
+}
+
+void Move(int x, int y, int distance)
+{
+    rope[0].X += x;
+    rope[0].Y += y;
+
+    for (var i = 1; i < rope.Length; i++)
+    {
+        var offset = new
+        {
+            X = rope[i - 1].X - rope[i].X,
+            Y = rope[i - 1].Y - rope[i].Y
+        };
+
+        void MoveX()
+        {
+            if (offset.X > 0)
+            {
+                rope[i].X++;
+            }
+            else if (offset.X < 0)
+            {
+                rope[i].X--;
+            }
+        }
+
+        void MoveY()
+        {
+            if (offset.Y > 0)
+            {
+                rope[i].Y++;
+            }
+            else if (offset.Y < 0)
+            {
+                rope[i].Y--;
+            }
+        }
+
+        if (offset.X > 1)
+        {
+            rope[i].X++;
+            MoveY();
+        }
+        else if (offset.X < -1)
+        {
+            rope[i].X--;
+            MoveY();
+        }
+        else if (offset.Y > 1)
+        {
+            rope[i].Y++;
+            MoveX();
+        }
+        else if (offset.Y < -1)
+        {
+            rope[i].Y--;
+            MoveX();
+        }
+    }
+
+    visited.Add(rope[^1]);
+
+    if (distance > 1)
+    {
+        Move(x, y, distance - 1);
+    }
+}
+
+Console.WriteLine(visited.Count);
