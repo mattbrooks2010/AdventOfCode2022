dfcc0cf0dcfe7062c1de1d21bf3b5910a4ae29d7
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-10 07:35:55 +0000

    Day 10

---
+++ Day-10a/Program.cs
@@ -0,0 +1,40 @@
+﻿var cpuX = 1;
+var addX = 0;
+var cycle = 0;
+var signal = 0;
+
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var clock = 0;
+
+    if (line == "noop")
+    {
+        clock = 1;
+        addX = 0;
+    }
+    else if (line.StartsWith("addx "))
+    {
+        clock = 2;
+        addX = int.Parse(line.Substring(5));
+    }
+
+    while (clock > 0)
+    {
+        clock--;
+        cycle++;
+
+        if (cycle == 20 || (cycle % 40) + 20 == 40)
+        {    
+            signal += cycle * cpuX;
+        }
+
+        if (clock == 0)
+        {
+            cpuX += addX;
+        }
+    }
+}
+
+Console.WriteLine(signal);
---
+++ Day-10b/Program.cs
@@ -0,0 +1,48 @@
+﻿var cpuX = 1;
+var addX = 0;
+var cycle = 0;
+
+var line = string.Empty;
+
+while ((line = Console.ReadLine()) != null)
+{
+    var clock = 0;
+
+    if (line == "noop")
+    {
+        clock = 1;
+        addX = 0;
+    }
+    else if (line.StartsWith("addx "))
+    {
+        clock = 2;
+        addX = int.Parse(line.Substring(5));
+    }
+
+    while (clock > 0)
+    {
+        var pos = cycle % 40;
+        
+        if (pos >= cpuX - 1 && pos <= cpuX + 1)
+        {
+            Console.Write('#');
+        }
+        else
+        {
+            Console.Write('.');
+        }
+
+        if (pos == 39)
+        {
+            Console.WriteLine(); 
+        }
+
+        clock--;
+        cycle++;
+
+        if (clock == 0)
+        {
+            cpuX += addX;
+        }
+    }
+}
