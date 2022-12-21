bf908c0974f774aff0e24d8ead2c88ff983087d9
Matt Brooks <mattbrooks2010@live.co.uk>
2022-12-21 20:21:38 +0000

    Day 20

---
+++ Day-20a/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllLines("input.txt")
+    .Select((line, index) => (num: int.Parse(line), idx: index))
+    .ToArray();
+
+var decrypted = input.ToList();
+
+foreach (var item in input)
+{
+    var index = decrypted.IndexOf(item);
+    decrypted.RemoveAt(index);
+    decrypted.Insert(((index + item.num % decrypted.Count) + decrypted.Count) % decrypted.Count, item);
+}
+
+var index0 = decrypted
+    .Select((o, i) => (o.num, idx: i))
+    .SkipWhile(o => o.num != 0)
+    .Select(o => o.idx)
+    .First();
+
+var index1 = decrypted[(index0 + 1000) % decrypted.Count].num;
+var index2 = decrypted[(index0 + 2000) % decrypted.Count].num;
+var index3 = decrypted[(index0 + 3000) % decrypted.Count].num;
+
+Console.WriteLine(index1 + index2 + index3);
---
+++ Day-20b/Program.cs
@@ -0,0 +1,29 @@
+﻿var key = 811589153;
+
+var input = File.ReadAllLines("input.txt")
+    .Select((line, index) => (num: long.Parse(line) * key, idx: index))
+    .ToArray();
+
+var decrypted = input.ToList();
+
+for (var i = 1; i <= 10; i++)
+{
+    foreach (var item in input)
+    {
+        var index = decrypted.IndexOf(item);
+        decrypted.RemoveAt(index);
+        decrypted.Insert(((int)(index + item.num % decrypted.Count) + decrypted.Count) % decrypted.Count, item);
+    }
+}
+
+var index0 = decrypted
+    .Select((o, i) => (o.num, idx: i))
+    .SkipWhile(o => o.num != 0)
+    .Select(o => o.idx)
+    .First();
+
+var index1 = decrypted[(index0 + 1000) % decrypted.Count].num;
+var index2 = decrypted[(index0 + 2000) % decrypted.Count].num;
+var index3 = decrypted[(index0 + 3000) % decrypted.Count].num;
+
+Console.WriteLine(index1 + index2 + index3);
